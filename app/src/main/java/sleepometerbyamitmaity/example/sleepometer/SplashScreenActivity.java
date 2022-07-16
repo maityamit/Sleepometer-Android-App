852f228568b34e0c75559fa1defdf35c51be927e
vicky <vickyrules1705@gmail.com>
2022-07-16 14:15:53 +0530

    animation implemented on splash activity

--- app/src/main/java/sleepometerbyamitmaity/example/sleepometer/SplashScreenActivity.java
+++ app/src/main/java/sleepometerbyamitmaity/example/sleepometer/SplashScreenActivity.java
@@ -10,15 +10,21 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreenActivity extends AppCompatActivity {
     private boolean connected = false;
-
     private FirebaseAuth mAuth;
     private String currentUserID;
+    private int Splash_screen=3000;
+    public  Animation top_welcome_anim,bottom_welcome_anim;
+
 
 
     @Override
@@ -30,6 +36,20 @@ public class SplashScreenActivity extends AppCompatActivity {
         //This method is used so that your splash activity
         //can cover the entire screen.
         setContentView(R.layout.activity_splash_screen);
+
+        //widgets reference
+        ImageView iconAPP=(ImageView)findViewById(R.id.splashIcon);
+        TextView appName=(TextView)findViewById(R.id.splashAppName);
+        TextView appSubName=(TextView)findViewById(R.id.splashSubText);
+
+        //setting animation to imageView and Textview
+        top_welcome_anim= AnimationUtils.loadAnimation(this,R.anim.welcome_top_animation);
+        bottom_welcome_anim= AnimationUtils.loadAnimation(this,R.anim.welcome_bottom_animation);
+
+        iconAPP.setAnimation(top_welcome_anim);
+        appName.setAnimation(bottom_welcome_anim);
+        appSubName.setAnimation(bottom_welcome_anim);
+
 
         mAuth = FirebaseAuth.getInstance ();
 
