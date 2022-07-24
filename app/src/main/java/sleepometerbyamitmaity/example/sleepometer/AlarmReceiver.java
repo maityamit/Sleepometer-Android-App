67be71d6d09bde951a9a0359994bde6ceee3f2db
JOYPAUL2001 <joydeepp2001@gmail.com>
2022-07-24 23:56:44 +0530

    added alarm feature

---
+++ app/src/main/java/sleepometerbyamitmaity/example/sleepometer/AlarmReceiver.java
@@ -0,0 +1,37 @@
+package sleepometerbyamitmaity.example.sleepometer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    // implement onReceive() method
+    public void onReceive(Context context, Intent intent) {
+
+        // we will use vibrator first
+        Vibrator vibrator = (Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
+        vibrator.vibrate(4000);
+
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+
+        // setting default ringtone
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+
+        // play ringtone
+        ringtone.play();
+    }
+}
